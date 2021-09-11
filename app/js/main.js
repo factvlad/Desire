ad7bd008bcdaaae0bc0e4b28a70a4cb8226d5fab
Vlad <fact1@ukr.net>
2021-09-11 11:42:35 +0300

    Final

--- app/js/main.js
+++ app/js/main.js
@@ -33,22 +33,44 @@ $(function () {
         settings: {
           slidesToShow: 8,
           slidesToScroll: 8,
-        }
+        },
       },
       {
         breakpoint: 1511,
         settings: {
           slidesToShow: 6,
           slidesToScroll: 6,
-        }
-      },{
-        breakpoint: 480,
+        },
+      },
+      {
+        breakpoint: 1201,
         settings: {
-          
-        }
-      }
-
-    ]
+          slidesToShow: 4,
+          slidesToScroll: 4,
+        },
+      },
+      {
+        breakpoint: 841,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+        },
+      },
+      {
+        breakpoint: 551,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+        },
+      },
+      {
+        breakpoint: 376,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
   });
 
   $(".article-slider__box").slick({
